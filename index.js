b61039c791f00ddbb0237c175042bc5a82cd8154
mathieu bortolaso <mathieu.bortolaso@carma.com>
2020-06-12 19:09:33 +0200

    clean unused code

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ function orderArticles(a, b) {
 
 
 projectWithWeirdSpace = [143,165,207,257,348,355,367,429,477,478,512,546,594,609,612,620,624,656,681,715,767,872,920,923,932,934,984,1021,1049,1092,1179,1245,1270,1284,1395,1409,1412,1445,1449,1462,1474,1484,1511,1523,1542,1544,1578,1604,1632,1640,1660,1665,1722,1729,1734,1742,1744,1750,1825,1834,2159,2628,2716,2871,2938,3092,3097,3195,3227,3228,3491,3522,3605,3694,3781,3979,4045,4167,4170,4220,4259,4263,4272,4273,4436,4498,5045,5191,5204,5654,5729,5940,6006,6007,6009,6012,6221,6237,6358,6639,6647,6680,6871,7188,7258,7289,7414,7479,7509,7593,7619,7629,7711,7882,7886,7904,7920,7970,7990,7991,8140,8141,8144,8195,8211,8255,8258,8287,8375,8377,8392,8400,8436,8446,8448,8499,8688,8718,8791,8792,8794,8881,8936,8937,8956,9016,9157,9199,9241,9308,9318,9320,9375,9378,9394,9399,9443,9470,9482,9524,9716,9733,9773,9781,9787,9812,9866,9879,9884,9886,9901,9906,9941,9958,9962,9964,9979,9980,10016,10087,10091,10094,10131,10174,10203,10243,10270,10377,10461,10510,10799,10800,10819,10839,10852,10869,10911,10912,10913,10933,10942,10964,11012,11014,11037,11038,11047,11048,11050,11115,11228,11285,11324,11329,11337,11404,11550,11553,11619,11624,11651,11657,11811,11821,11844,11845,12198,12201,12203,12204,12215,12300,12321,12467,12522,12547,12549,12564,12565,12599,12609,12614,12630,12636,12686,12690,12691,12694,12695,12729,12747,12749,12783,12796,12813]
-projinpast = [9418
+projinpast = [
+   /* 9418
     ,12811
     ,7269
     ,10387
@@ -148,7 +149,7 @@ projinpast = [9418
     ,8201
     ,8199
     ,1101,
-    8197, 12809]
+8197, 12809*/]
 
 const weirdQuote = [12583];
 const spaceAtTheEnd = [10016, 6006, 5281];
@@ -247,12 +248,6 @@ function compare(articlesNode, articlesRuby) {
         console.error("node_minus_ruby", node_minus_ruby); // 438, 468 454 164
         console.error("some articles are not in both queue");
 
-//         "污"
-// 1:"染"
-// 2:"不"
-// 3:"合"
-// 4:"格"
-
         console.log("the first article in ruby that is not in node is ", articlesRuby.find(function(art, i){
 
             if(art._id == ruby_minus_node[toDebug + 1]){
@@ -272,23 +267,9 @@ function compare(articlesNode, articlesRuby) {
             // if(art.article_language == "en"){
                 if(["5d8a148d4672a9451e770480","5d8a14ec4672a9451e7710d5","5d8a146d4672a9451e77006b"].indexOf(id) > 0) return;
                 art.relevancy.forEach(function name(rel) {
-                    // if(rel.hits.indexOf("and") < 0 && rel.hits.indexOf("at") < 0 && rel.hits.indexOf("to") && rel.hits.indexOf("ARCADIA") < 0){
-                    //     if(rel.hits.indexOf("Trump") < 0 && rel.hits.indexOf("SOCCER") < 0){
-                    //         console.log("test")
-                    //         // debugger;
-                    //     }
-                    // }
                     rel.hits.forEach(function(hit, j){
-                        // if(["FDA","food","reveal","media","coverage", "WeChat","memory","Microsoft", "不合","回收","污染", "ice", "recall", "additives","格","记","忆","II"].indexOf(hit) <= 0){
-                        //     console.log("match", rel, art,id)
-                        // };
-                        // if(hit == "food"){
-                        //     console.log("match", rel, art,id, lll)
-                        // }
                         words2[hit] =  1 + (words2[hit] || 0)
                     })
-                    console.log("-9--")
-                    // console.log(rel.hits[0]);
                 })
             // }
             Object.keys(words2).forEach(function name(word) {
@@ -306,19 +287,29 @@ function compare(articlesNode, articlesRuby) {
     const relevancyNode = {};
     articlesNode.forEach(function(aNode){
         const id = aNode._id;
+        if(["5d8a143b4672a9451e76fab4"].indexOf(id) >= 0){
+            console.log("goodman missing");
+        }
+        if(["5ee34c5fbeca882d7edaaa63"].includes(id)) {
+            console.error("why ruby does not match this one?")
+            return false;
+            // console.log(aNode);
+        }
+        if("5ee34c7bbeca882d7edaab62" == id) {
+            console.error("why ruby does not match this one? but node have weird hit they should be merged")
+            return false;
+        }
+        if(["5ee34c7bbeca882d7edaab67"].includes(id)) {
+            console.log("Ruby is wrong to not match this one there is \"GE\' in the article");
+            return false;
+        }
         aNode.projects.forEach(function (proj) {
             nodeProject[proj] = nodeProject[proj] || [];
             nodeProject[proj].push(id);
         })
-        if(["5d8a143b4672a9451e76fab4"].indexOf(id) >= 0){
-            console.log("goodman missing");
-        }
         aNode.relevancy.forEach(function name(rel) {
             rel.hits.forEach(function(hit){
                 hit = hit.toLowerCase();
-                // if(hit == "typhoon"){  //there is also Eurofighter so it it normal it match
-                //     console.log("typhoon", aNode, rel);
-                // }
                 relevancyNode[hit] = relevancyNode[hit] || [];
                 relevancyNode[hit].push(id);
             })
@@ -331,28 +322,14 @@ function compare(articlesNode, articlesRuby) {
     const relevancyRuby = {};
     articlesRuby.forEach(function(aRuby){
         const id = aRuby._id;
-        if(["5d8a14414672a9451e76fb6c"].indexOf(id) >= 0){
-            console.log("the project is inactive so it is normal node don't percolate it");
+        if(["5ee34c5bbeca882d7edaaa3a"].includes(id)) {
+            console.log("Ruby is wrong: why aluminium matches, it should only match if there is Alba");
             return false;
         }
-        if(["5d8a14424672a9451e76fb8f", "5d8a14b94672a9451e770a3f"].indexOf(id) >= 0){
-            console.log("I don't understand why ruby match this document");
+        if(["5ee34c61beca882d7edaaa76"].includes(id)) {
+            console.log("Ruby is wrong \"or\" here is not a word to match but an operator");
             return false;
         }
-        if(["5d8a14df4672a9451e770f3a"].indexOf(id) >= 0){
-            console.log("it is written Retail AND health but there is only health, but there is the character U+A0 that break the query for ruby on the project 6012");
-            return false;
-        }
-        if(["5d8a14b74672a9451e7709f7","5d8a14b94672a9451e770a31","5d8a14c04672a9451e770b21","5d8a14dd4672a9451e770eed","5d8a14dd4672a9451e770ee4","5d8a14dc4672a9451e770ecf","5d8a14dc4672a9451e770ec2","5d8a14db4672a9451e770e9a"].indexOf(id) >= 0) {
-            console.log("invisible character like 5d8a14df4672a9451e770f3a");
-            return false;
-        }
-        if(["5d8a14e64672a9451e77100c"].indexOf(id) >= 0) {
-            console.log(`Node transform NOT into "AND NOT" while Ruby were ignoring the NOT if there is not and before`);
-            return false;
-        }
-        // let key = aRuby.projects.sort((a, b) => a > b ? 1 : -1).join("-->");
-        // rubyKey.add(id + "@" + key);
         aRuby.projects.forEach(function (proj) {
             rubyProject[proj] = rubyProject[proj] || [];
             rubyProject[proj].push(id);
@@ -412,14 +389,14 @@ function compare(articlesNode, articlesRuby) {
     Object.entries(relevancyRuby).forEach(function([key, value]){
         if(!relevancyNode[key]){
             diffsRubyLessNodeRelevancy.push(key);
-            console.log("the keyword ", key, " is not in node");
+            console.log("the keyword ", key, " is not in node", value);
         }
     })
     let diffsNodeLessRubyRelevancy = [];
     Object.entries(relevancyNode).forEach(function([key, value]){
         if(!relevancyRuby[key]){
             diffsNodeLessRubyRelevancy.push(key);
-            console.log("the keyword ", key, " is not in node");
+            console.log("the keyword ", key, " is not in ruby, article_id", value);
         }
     })
     diffsRubyLessNodeRelevancy = diffsRubyLessNodeRelevancy.filter((hit) => !hasBeenManuallyValidate.has(hit));
@@ -500,8 +477,9 @@ function compare(articlesNode, articlesRuby) {
     });
 
     let iij = 0;
-    let cont = 0;
     const toCheck = [];
+
+    // compare the ruby and node article
     for(let arti of artiNodeRuby){
         const _a = arti.node, _b = arti.ruby;
         // 10995 space at the end of request
@@ -509,50 +487,12 @@ function compare(articlesNode, articlesRuby) {
             ++iij
         }
         if(arti.ruby != arti.node){
-            //
+            console.error("difference between node and ruby");
         }
         if(arti.ruby.length > arti.node.length){
             ++iij;
             if(arti.ruby.indexOf(11143) >= 0){
                 console.log("11443");
-            }
-            if(arti.nodeObj._id == "5d8a148c4672a9451e77045d") {
-                console.log("node is right there is shell in the headline");
-                cont++;continue;
-            }
-            if(["5d8a14a34672a9451e77074b", "5d8a144a4672a9451e76fc96"].indexOf(arti.nodeObj._id) >= 0) {
-                console.log("twice the same tag is not a big deal");
-                cont++;continue;
-            }
-            if(["5d8a14d84672a9451e770e38"].indexOf(arti.nodeObj._id) >= 0) {
-                console.log("there is not reason to have villeroy as hit even with the *");
-                cont++;continue;
-            }
-            if(["5d8a14414672a9451e76fb74", "5d8a14524672a9451e76fd76","5d8a14884672a9451e7703cd", "5d8a14d24672a9451e770d6c"].indexOf(arti.nodeObj._id) >= 0) {
-                console.error("reactivate the patch for and can solve this problem");
-                cont++;continue;
-            }
-
-            if(["5d8a14824672a9451e770302"].indexOf(arti.nodeObj._id) >=0) {
-                console.log("the article match the project 10138 and it is normal");
-                cont++;continue;
-            }
-            if(["5d8a146a4672a9451e770005"].indexOf(arti.nodeObj._id) >=0) {
-                console.log("ruby put in hit word that are in the node condition, this is not logic");
-                cont++;continue;
-            }
-            if(["5d8a14504672a9451e76fd48", "5d8a144c4672a9451e76fce9","5d8a14fa4672a9451e7712c0","5d8a147c4672a9451e77024d"].indexOf(arti.nodeObj._id) >=0) {
-                console.log("relevancy is in 2 object instead of one but it is the same");
-                cont++;continue;
-            }
-
-            if(["5d8a14454672a9451e76fbfd"].indexOf(arti.nodeObj._id) >=0) {
-                console.log("there is a weird quote in the query 2783 project 12583");
-                cont++;continue;
-            }
-            if(["5d8a14404672a9451e76fb43"].indexOf(arti.nodeObj._id) >=0) {
-                console.log("except the question mark it is the same");
-                cont++;continue;
             }
 
             if(arti.node != arti.ruby) {
@@ -562,177 +502,12 @@ function compare(articlesNode, articlesRuby) {
         }
     }
     console.log(toCheck);
-    console.log(artiNodeRuby);
-
-
-    // function extractWord(obj){
-    //     let str = obj
-    //     if(obj instanceof Object){
-    //         str = JSON.stringify(obj)
-    //     }
-    //     var re = /\s*([^[:]+):\"([^"]+)"/g;
-    //     var m;
-    //     while (m = re.exec(s)) {
-    //     console.log(m[1], m[2]);
-    //     }
-    // }
-
-    // let nbSucceed = 0;
-    // for(let i = 0; i < articlesNode.length; ++i) {
-    //     const strA = stringify(filterWhatCanNotBeCompared(articlesNode[i]));
-    //     const strB = stringify(filterWhatCanNotBeCompared(articlesRuby[i]));
-    //     if(!strA.length || !strB.length){
-    //         console.error("problem while stringify");
-    //         debugger;
-    //     }
-        
-    //     if(strA != strB){
-    //         console.error("strA and strB are different");
-    //         console.error("strA = " + strA);
-    //         console.error("strB = " + strB);
-    //         debugger;
-    //     }else{
-    //         ++nbSucceed;
-    //     };
-    // }
-    // if(!nbSucceed || nbSucceed < articlesNode.length){
-    //     console.error("no success", nbSucceed, articlesNode.length);
-    //     debugger;
-    // }
+    console.log("this array contain the ruby and node article for the same id", artiNodeRuby);
     console.log("finished");
 }
 
-const strNode = fs.readFileSync("./articlesNode.json");
-const strRuby = fs.readFileSync("./articlesRuby.json");
-
-const articlesNode = JSON.parse(strNode);
-let ind = JSON.stringify(articlesNode.body.searches).indexOf("EMPOWERING")
-console.log(JSON.stringify(articlesNode.body.searches).slice(ind-20, ind + 50))
-
-request.post({
-    url: "http://localhost:9292/process",
-    headers: articlesNode.headers,
-    json: articlesNode.body
-}, function(err, res, body) {
-    const error = err || body.error
-    if (error) {
-        console.error(error);    
-    }
-    console.log(body)
-    const stringifyExpected = stringify({
-        "10834": 
-        {
-            "hits": [
-                "company",
-                "her",
-                "that",
-                "the",
-                "time"
-            ],
-        },
-        "11999": 
-        {
-            "hits": [
-                "innovation"
-            ],
-            "tags": [
-                
-                {
-                    "category": "Products",
-                    "name": "Innovation",
-                }
-            ],
-        },
-        "7414": 
-        {
-            "hits": [
-                "EMPOWERING"
-            ],
-            "tags": [
-                
-                {
-                    "category": "Places",
-                    "name": "MANPOWER",
-                }
-            ],
-        },
-        "916": 
-        {
-            "hits": [
-                "innovation"
-            ],
-            "tags": [
-                
-                {
-                    "category": "Products",
-                    "name": "Innovation",
-                }
-            ],
-        },
-        "9655": 
-        {
-            "hits": [
-                "law"
-            ],
-            "tags": [
-                
-                {
-                    "category": "Products",
-                    "name": "Tobacco",
-                }
-            ],
-        },
-        "article_tags": [
-            
-            {
-                "category": "Brand",
-                "id": 1522,
-            }
-        ]
-    })
-    if(stringify(body) != stringifyExpected){
-        console.error("result is different")
-    }else{
-        console.log("result is Ok")
-    }
-    debugger
-});
-const data = JSON.stringify(articlesNode.body);
-  
-// const options = {
-//     hostname: "localhost",
-//     port: 9292,
-//     path: "/process",
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Content-Length': data.length
-//     }
-// }
-  
-// resu = ""
-// const http = require("http")
-// const req = http.request(options, (res) => {
-//     console.log(`statusCode: ${res.statusCode}`)
-//     res.on('data', (d) => {
-//         resu += d.toString("binary")
-//     })
-// })
-
-// req.on('error', (error) => {
-//     console.error(error)
-// })
-
-// req.on('end', () => {
-//     try{
-//         toto = JSON.parse(toto);
-//     }catch(e){}
-//     console.log(toto);
-// })
-
-// req.write(data)
-// req.end()
-
-// const articlesRuby = JSON.parse(strRuby);
-
-// compare(articlesNode, articlesRuby);
+let strNode = fs.readFileSync("./mb_regex_tagged_node");
+let strRuby = fs.readFileSync("./mb_regex_tagged");
+const articlesNode = eval("strNode = " + strNode);
+const articlesRuby = eval("strRuby = " + strRuby);
+compare(articlesNode, articlesRuby);
